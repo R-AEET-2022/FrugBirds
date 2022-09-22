7cbfc5baf417195209545f9238bbaf03da9e27c8
iciarjimm <iciarjm@gmail.com>
2022-09-22 18:03:04 +0200

    create makefile script

---
+++ Script/makefile.R
@@ -0,0 +1,13 @@
+#This script is to run the scripts in order to make this project reproducible
+
+#First script: clean data from AVONET database
+
+source("Script/AVONET_DataCleaning.R")
+
+#Second script: clean data from Elton Traits
+
+source("Script/Elton_DataCleaning.R")
+
+#Third script: exploratory analysis
+
+
